1b9679aca3bf2eb97b9a58833f66f6eae5bf0a53
Sahil Pate <sahil@abovethefray.io>
2024-11-15 16:47:56 +0530

    inner header issue fixed

--- components/innerHeader.tsx
+++ components/innerHeader.tsx
@@ -1,41 +1,39 @@
 import { Box, Button, H1, HR, Text } from '@bigcommerce/big-design';
 import { ArrowBackIcon } from '@bigcommerce/big-design-icons';
 import { useRouter } from 'next/router';
-import { useProductList } from '../lib/hooks';
+import {useEffect, useState} from "react";
+import { useProductList } from '@lib/hooks';
 import { TabIds, TabRoutes } from './header';
-import {string} from "prop-types";
 
 const InnerHeader = () => {
     const router = useRouter();
+    const { pathname } = router;
     const { pid } = router.query;
     const { list = [] } = useProductList();
     const { name } = list.find(item => item.id === Number(pid)) ?? {};
-
-    const InnerRoutes = [
-        '/products/[pid]',
-        '/customers/[pid]',
-    ];
+    const [title, setTitle] = useState("");
    
     const handleBackClick = () =>{ 
-        if (InnerRoutes.includes('/products/[pid])')){
+        if (pathname.includes('/products/[pid]')){
             router.push(TabRoutes[TabIds.PRODUCTS]);
-        } else if (InnerRoutes.includes('/customers/[pid]')){
+            
+        } else if (pathname.includes('/customers/[pid]')){
             router.push(TabRoutes[TabIds.CUSTOMERS]);
         }
     };
-    
-    const title = ()=>{
-        if (InnerRoutes.includes('/products/[pid])')){
-            return 'Products';
-        } else if (InnerRoutes.includes('/customers/[pid]')){
-            return 'Customers';
+
+    useEffect(() => {
+        if (String(pathname) === '/products/[pid]'){
+            setTitle('Products');
+        } else if (String(pathname) === '/customers/[pid]'){
+            setTitle('Customers')
         }
-    }
+    }, [pathname]);
 
     return (
         <Box marginBottom="xxLarge">
             <Button iconLeft={<ArrowBackIcon color="secondary50" />} variant="subtle" onClick={handleBackClick}>
-                <Text bold color="secondary50">{title()}</Text>
+                <Text bold color="secondary50">{title}</Text>
             </Button>
             {name &&
                 <H1>{name}</H1>
